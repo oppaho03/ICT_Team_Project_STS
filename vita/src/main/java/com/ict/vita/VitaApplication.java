0b33dc291e440768e20e403fee8d966282de58ea
oppaho03 <oppaho03@gmail.com>
2025-02-17 17:44:46 +0900

    Package initialization

---
+++ vita/src/main/java/com/ict/vita/VitaApplication.java
@@ -0,0 +1,13 @@
+package com.ict.vita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaApplication.class, args);
+	}
+
+}
---
+++ vita/src/test/java/com/ict/vita/VitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.vita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
