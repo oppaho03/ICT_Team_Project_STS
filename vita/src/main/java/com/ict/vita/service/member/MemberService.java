67b912810439fe68a3bd92a51900a80c907cd42f
HwangJiHo <ghkdwlgh123@naver.com>
2025-02-26 20:27:18 +0900

    회원가입 처리중..... otl....

--- vita/src/main/java/com/ict/vita/service/member/MemberService.java
+++ vita/src/main/java/com/ict/vita/service/member/MemberService.java
@@ -3,6 +3,7 @@ package com.ict.vita.service.member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ict.vita.repository.member.MemberEntity;
 import com.ict.vita.repository.member.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,5 +15,53 @@ public class MemberService {
 	//리포지토리 주입
 	private final MemberRepository memberRepository;
 	
+	//이메일 중복 여부 판단
+	public boolean isExistsEmail(String email) {
+		return memberRepository.existsByEmail(email);
+	}
 	
+	////전화번호 중복 여부 판단
+	public boolean isExistsContact(String contact) {
+		return memberRepository.existsByContact(contact);
+	}
+	
+	/*
+	//회원가입 처리
+	public MemberDto signup(MemberDto dto) {
+		//이메일 중복 여부 판단
+		boolean isExistingEmail = memberRepository.existsByEmail(dto.getEmail());
+		//전화번호 중복 여부 판단
+		boolean isExistingContact = memberRepository.existsByContact(dto.getContact());
+		
+		//이메일,전화번호가 중복되지않은 경우 
+		//[회원가입 처리]
+		if(!(isExistingEmail || isExistingContact)) {
+			//회원 닉네임 설정
+			String nickname = "";
+			if(dto.getNickname() == null | dto.getNickname().trim().length() == 0) { //닉네임을 입력 안 한 경우
+				nickname = dto.getNickname().substring(0, dto.getNickname().indexOf("@")); //@ 전까지를 닉네임으로 지정
+				System.out.println("MemberService 회원가입 - 회원 닉네임: "+nickname);
+			}
+			//회원 저장
+			MemberEntity entity = memberRepository.save(MemberEntity.builder()
+												.email(dto.getEmail())
+												.password(dto.getPassword())
+												.name(dto.getName())
+												.nickname(nickname)
+												.birth(dto.getBirth())
+												.gender(dto.getGender())
+												.contact(dto.getContact())
+												.address(dto.getAddress())
+												.token("test토큰입니다") //테스트용 토큰
+												.status(1) //회원가입:1 / 탈퇴:0 / 대기:9
+												.build()
+			);
+			return MemberDto.toDto(entity);
+		}///if
+		
+		//이메일이나 전화번호가 중복되어
+		//[회원가입이 불가능한 경우]
+		return null;
+	}
+	*/
 }
