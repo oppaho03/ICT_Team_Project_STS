8aac64718905c924322101f14fabd1b5435cd0d4
jiho0107 <ghkdwlgh123@naver.com>
2025-02-28 02:25:53 +0900

    회원가입 테스트중...ㅠㅠ

--- vita/src/main/java/com/ict/vita/controller/member/MemberController.java
+++ vita/src/main/java/com/ict/vita/controller/member/MemberController.java
@@ -1,16 +1,35 @@
 package com.ict.vita.controller.member;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ict.vita.service.member.MemberDto;
 import com.ict.vita.service.member.MemberService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
 	//서비스 주입
 	private final MemberService memberService;
 	
-	
+	/**
+	 * 회원가입
+	 * @param member 회원정보를 담은 객체
+	 * @param errors
+	 * @return
+	 */
+	@PostMapping("member")
+	public ResponseEntity<Object> join(@RequestBody MemberDto member){
+		//회원가입 테스트!!!!
+		return null;
+	}
 }
--- vita/src/main/java/com/ict/vita/service/member/MemberService.java
+++ vita/src/main/java/com/ict/vita/service/member/MemberService.java
@@ -15,9 +15,8 @@ public class MemberService {
 	//리포지토리 주입
 	private final MemberRepository memberRepository;
 	
-	//이메일 중복 여부 판단
 	/**
-	 * 
+	 * 이메일 중복 여부 판단
 	 * @param email 입력된 이메일 주소
 	 * @return 가입된 이메일이 존재하는지 여부
 	 */
@@ -25,9 +24,8 @@ public class MemberService {
 		return memberRepository.existsByEmail(email);
 	}
 	
-	////전화번호 중복 여부 판단
 	/**
-	 * 
+	 * 전화번호 중복 여부 판단
 	 * @param contact 입력된 전화번호
 	 * @return 가입된 전화번호가 존재하는지 여부
 	 */
@@ -35,43 +33,57 @@ public class MemberService {
 		return memberRepository.existsByContact(contact);
 	}
 	
-	/*
-	//회원가입 처리
-	public MemberDto signup(MemberDto dto) {
-		//이메일 중복 여부 판단
-		boolean isExistingEmail = memberRepository.existsByEmail(dto.getEmail());
-		//전화번호 중복 여부 판단
-		boolean isExistingContact = memberRepository.existsByContact(dto.getContact());
-		
-		//이메일,전화번호가 중복되지않은 경우 
+	
+	/**
+	 * 회원가입 처리
+	 * @param member 회원정보를 담은 객체
+	 * @return
+	 */
+	public MemberDto signup(MemberDto member) {	
 		//[회원가입 처리]
-		if(!(isExistingEmail || isExistingContact)) {
-			//회원 닉네임 설정
-			String nickname = "";
-			if(dto.getNickname() == null | dto.getNickname().trim().length() == 0) { //닉네임을 입력 안 한 경우
-				nickname = dto.getNickname().substring(0, dto.getNickname().indexOf("@")); //@ 전까지를 닉네임으로 지정
-				System.out.println("MemberService 회원가입 - 회원 닉네임: "+nickname);
-			}
+		//회원 닉네임 설정
+		String nickname = "";
+		//<닉네임 미입력시>
+		if(member.getNickname() == null || member.getNickname().isBlank()) { //닉네임을 입력 안 한 경우
+			nickname = member.getNickname().substring(0, member.getNickname().indexOf("@")); //@ 전까지를 닉네임으로 지정
+			System.out.println("MemberService 회원가입 - 회원 닉네임: "+nickname);
 			//회원 저장
 			MemberEntity entity = memberRepository.save(MemberEntity.builder()
-												.email(dto.getEmail())
-												.password(dto.getPassword())
-												.name(dto.getName())
-												.nickname(nickname)
-												.birth(dto.getBirth())
-												.gender(dto.getGender())
-												.contact(dto.getContact())
-												.address(dto.getAddress())
-												.token("test토큰입니다") //테스트용 토큰
-												.status(1) //회원가입:1 / 탈퇴:0 / 대기:9
-												.build()
-			);
+								.email(member.getEmail())
+								.password(member.getPassword())
+								.role(member.getRole())
+								.name(member.getName())
+								.nickname(nickname)
+								.birth(member.getBirth())
+								.gender(member.getGender())
+								.contact(member.getContact())
+								.address(member.getAddress())
+								.token(member.getToken())
+								.created_at(member.getCreated_at())
+								.updated_at(member.getUpdated_at())
+								.status(1) //회원가입:1 / 탈퇴:0 / 대기:9
+								.build());
 			return MemberDto.toDto(entity);
-		}///if
+		}
+		//<닉네임 입력시>
+		//회원 저장
+		MemberEntity entity = memberRepository.save(MemberEntity.builder()
+								.email(member.getEmail())
+								.password(member.getPassword())
+								.role(member.getRole())
+								.name(member.getName())
+								.nickname(member.getNickname())
+								.birth(member.getBirth())
+								.gender(member.getGender())
+								.contact(member.getContact())
+								.address(member.getAddress())
+								.token(member.getToken())
+								.created_at(member.getCreated_at())
+								.updated_at(member.getUpdated_at())
+								.status(1) //회원가입:1 / 탈퇴:0 / 대기:9
+								.build());
+		return MemberDto.toDto(entity);
 		
-		//이메일이나 전화번호가 중복되어
-		//[회원가입이 불가능한 경우]
-		return null;
 	}
-	*/
+	
 }
