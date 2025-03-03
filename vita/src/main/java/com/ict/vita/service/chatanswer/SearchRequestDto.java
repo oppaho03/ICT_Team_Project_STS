b1dbffb1329bc4b6951b1507401ed7b7aa41d3d5
HwangJiHo <ghkdwlgh123@naver.com>
2025-03-03 14:36:43 +0900

    (hjh) 답변 검색을 위한 SearchRequestDto객체 생성

---
+++ vita/src/main/java/com/ict/vita/service/chatanswer/SearchRequestDto.java
@@ -0,0 +1,35 @@
+package com.ict.vita.service.chatanswer;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+//[키워드로 검색시 서버에서 키워드를 받을 DTO 객체]
+/* 
+	 { "data" : [
+	         {"keyword" : "검사"},
+	         {"keyword" : "항원"},
+	             .........
+	   ]   
+	 } 
+	 와 같은 json형식으로 데이터 받음
+*/
+public class SearchRequestDto {
+	private List<Keyword> data;
+	
+	//내부 클래스
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Keyword{ //내부클래스 static으로 안 하면 에러남
+		String keyword;
+	}
+}
