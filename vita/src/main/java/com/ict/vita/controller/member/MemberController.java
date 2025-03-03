44b2729da9de2d282490db9a7922237df8e8bce8
HwangJiHo <ghkdwlgh123@naver.com>
2025-03-03 17:14:27 +0900

    (hjh).....

--- vita/src/main/java/com/ict/vita/controller/member/MemberController.java
+++ vita/src/main/java/com/ict/vita/controller/member/MemberController.java
@@ -28,6 +28,7 @@ import com.ict.vita.util.Result;
 import com.ict.vita.util.ResultUtil;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
