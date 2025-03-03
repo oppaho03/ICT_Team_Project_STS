04205b2f84ddd5d0cfa542b2cc5a8e3e9402d0a4
HwangJiHo <ghkdwlgh123@naver.com>
2025-03-03 17:05:44 +0900

    (hjh)답변 키워드 쿼리 수정

--- vita/src/main/java/com/ict/vita/repository/chatanswer/ChatAnswerRepository.java
+++ vita/src/main/java/com/ict/vita/repository/chatanswer/ChatAnswerRepository.java
@@ -11,18 +11,18 @@ import org.springframework.stereotype.Repository;
 public interface ChatAnswerRepository extends JpaRepository<ChatAnswerEntity, Long>{
 	
 	@Query(value = """
-		    SELECT scores,id
-		    FROM (
-		        SELECT a.*, SCORE(1) + SCORE(2) + SCORE(3) AS scores
-		        FROM APP_CHAT_ANSWER a
-		        WHERE 
-		            CONTAINS(a.intro, :keywords, 1) > 0
-		            OR CONTAINS(a.body, :keywords, 2) > 0
-		            OR CONTAINS(a.conclusion, :keywords, 3) > 0
-		        ORDER BY scores DESC
-		    ) 
-		    WHERE ROWNUM <= 5
-		    """, nativeQuery = true)
+            SELECT aa.id, aa.file_name, aa.intro, aa.body, aa.conclusion
+            FROM (
+                SELECT a.id, a.file_name, a.intro, a.body, a.conclusion, SCORE(1) + SCORE(2) + SCORE(3)  AS scores
+                FROM APP_CHAT_ANSWER a
+                WHERE
+                    CONTAINS(a.intro, :keywords, 1) > 0
+                    OR CONTAINS(a.body, :keywords, 2) > 0
+                    OR CONTAINS(a.conclusion, :keywords, 3) > 0
+                ORDER BY scores DESC
+            ) aa
+            WHERE ROWNUM <= 5
+            """, nativeQuery = true)
 	List<ChatAnswerEntity> findAnswerByKeywords(@Param("keywords") String keywords); //키워드로 답변 검색
 
 }
