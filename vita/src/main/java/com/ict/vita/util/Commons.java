d425c7a6a4a9a67dbbc62e35aee9387b5fa92c1c
oppaho03 <oppaho03@gmail.com>
2025-03-04 16:26:14 +0900

    Config 파일 BindingResult 포맷터 추가가

--- vita/src/main/java/com/ict/vita/util/Commons.java
+++ vita/src/main/java/com/ict/vita/util/Commons.java
@@ -1,5 +1,9 @@
 package com.ict.vita.util;
 
+import java.util.stream.Collectors;
+
+import org.springframework.validation.BindingResult;
+
 public class Commons {
 	/**
 	 * 문자열이 null 또는 빈문자열인지 판단하는 함수
@@ -8,6 +12,34 @@ public class Commons {
 	 */
 	public static boolean isNull(String string) {
 		return ( string == null || string.isEmpty() || string.trim().length() == 0 ) ? true : false;
+	}
+
+	/**
+	 * BindingResult 결과 포맷 
+	 * @param bindingResult BindingResult
+	 * @param message String
+	 * @return 
+	 */
+	public static String formatBindingResultHasError( BindingResult bindResult ) {
+		return bindResult.hasErrors() ? String.format( 
+			"Invalid Error : "
+			+ bindResult.getAllErrors().stream().map( e -> e.getDefaultMessage() ).collect( Collectors.joining(",") )
+		) : null;
+	}
+
+	/**
+	 * BindingResult 결과 포맷 
+	 * @param bindingResult BindingResult
+	 * @param message String
+	 * @return 
+	 */
+	public static String formatBindingResultHasError( BindingResult bindResult, String message) {
+		if ( Commons.isNull(message) ) message = "Invalid Error";
+		return bindResult.hasErrors() ? String.format( 
+			"%s : "
+			+ bindResult.getAllErrors().stream().map( e -> e.getDefaultMessage() ).collect( Collectors.joining(",") )
+			, message
+		) : null;
 	}
 	
 }
