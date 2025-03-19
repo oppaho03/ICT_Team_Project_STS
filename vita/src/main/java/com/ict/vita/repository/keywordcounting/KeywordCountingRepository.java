7733e0695f2b2b9e015b26b279a0bac8e67d5680
KimDongHyeon <dhb6306@gmail.com>
2025-03-19 17:34:05 +0900

    키워드 카운팅 데이터 저장 구현

--- vita/src/main/java/com/ict/vita/repository/keywordcounting/KeywordCountingRepository.java
+++ vita/src/main/java/com/ict/vita/repository/keywordcounting/KeywordCountingRepository.java
@@ -3,7 +3,13 @@ package com.ict.vita.repository.keywordcounting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ict.vita.repository.termcategory.TermCategoryEntity;
+import com.ict.vita.repository.terms.TermsEntity;
+
 @Repository
 public interface KeywordCountingRepository extends JpaRepository<KeywordCountingEntity, Long> {
+
+	KeywordCountingEntity findByTermsEntityAndSearchedAt(TermsEntity entity, String serachedDate);
+
 
 }
