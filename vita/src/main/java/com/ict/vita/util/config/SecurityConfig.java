da4399c7d5280aaa83deb362582951f0132c4ecc
HwangJiHo <ghkdwlgh123@naver.com>
2025-03-21 19:16:03 +0900

    (HJH)WebClientTestController 완성중...

--- vita/src/main/java/com/ict/vita/util/config/SecurityConfig.java
+++ vita/src/main/java/com/ict/vita/util/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain( HttpSecurity http ) throws Exception {
-        http.csrf().disable().authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
     return http.build();
     }
 }
