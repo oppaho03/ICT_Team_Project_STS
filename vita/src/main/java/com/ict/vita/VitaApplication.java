925f47fe52e524d05de9173515977634fc9af35e
oppaho03 <oppaho03@gmail.com>
2025-03-21 16:53:13 +0900

    (PYS) 황지호 보아라라

--- vita/src/main/java/com/ict/vita/VitaApplication.java
+++ vita/src/main/java/com/ict/vita/VitaApplication.java
@@ -2,8 +2,9 @@ package com.ict.vita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityFilterAutoConfiguration.class)
 public class VitaApplication {
 
 	public static void main(String[] args) {
