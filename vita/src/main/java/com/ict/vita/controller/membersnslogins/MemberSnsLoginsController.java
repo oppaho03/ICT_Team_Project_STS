399ff31d7ca2aa7c374f2b1a8ab15fea585d1aef
HwangJiHo <ghkdwlgh123@naver.com>
2025-03-21 10:16:07 +0900

    (hjh)MemberSnsLoginsController 수정중중중.....

--- vita/src/main/java/com/ict/vita/controller/membersnslogins/MemberSnsLoginsController.java
+++ vita/src/main/java/com/ict/vita/controller/membersnslogins/MemberSnsLoginsController.java
@@ -48,7 +48,7 @@ public class MemberSnsLoginsController {
 		if(snsDto != null) {
 			//회원 테이블에 존재
 			if(findedMember.getStatus() != 1) { 
-				findedMember.setStatus(1);
+				findedMember.setStatus(1); //status를 1로(가입상태)
 				memberService.updateMember(findedMember);
 			}
 			return ResponseEntity.status(HttpStatus.OK).body(ResultUtil.success(snsDto));
@@ -64,28 +64,17 @@ public class MemberSnsLoginsController {
 									.nickname( snsReqDto.getEmail().trim().split("@")[0] )
 									.created_at(LocalDateTime.now())
 									.updated_at(LocalDateTime.now())
-									.status(1)
+									.status(1) //status를 1로(가입상태)
 									.build();
 			findedMember = memberService.join(findedMember);
-			//sns회원 테이블에 저장
-			snsDto = MemberSnsLoginsDto.builder()
-									.memberDto(findedMember)
-									.login_id( snsReqDto.getEmail().trim() )
-									.access_token(snsReqDto.getToken().trim())
-									.status(1)
-									.login_created_at(LocalDateTime.now())
-									.login_modified_at(LocalDateTime.now())
-									.provider(Commons.TEMPORARY)
-									//.provider_id(Commons.TEMPORARY) //유니크한 값
-									.build();
-			return ResponseEntity.status(HttpStatus.OK).body(ResultUtil.success(snsDto));
 		}
 		
-		//회원 테이블에 있을 때
-		if(findedMember.getStatus() != 1) {
-			findedMember.setStatus(1);
-			findedMember.setPassword(EncryptAES256.encrypt(snsReqDto.getToken()));
-			findedMember = memberService.updateMember(findedMember);
+		else { //회원 테이블에 있을 때
+			if(findedMember.getStatus() != 1) {
+				findedMember.setStatus(1); //status를 1로(가입상태)
+				findedMember.setPassword(EncryptAES256.encrypt(snsReqDto.getToken()));
+				findedMember = memberService.updateMember(findedMember);
+			}
 		}
 		
 		//sns회원 테이블에 저장
@@ -93,99 +82,16 @@ public class MemberSnsLoginsController {
 								.memberDto(findedMember)
 								.login_id( snsReqDto.getEmail().trim() )
 								.access_token(snsReqDto.getToken().trim())
-								.status(1)
+								.status(1) //status를 1로
 								.login_created_at(LocalDateTime.now())
 								.login_modified_at(LocalDateTime.now())
 								.provider(Commons.TEMPORARY)
 								//.provider_id(Commons.TEMPORARY) //유니크한 값
 								.build();
+		memberSnsLoginsService.save(snsDto);
+		
 		return ResponseEntity.status(HttpStatus.OK).body(ResultUtil.success(snsDto));
 		
-		
-		/*
-		//<회원 테이블에서 이메일 존재여부 확인>
-		//회원 존재시
-		if(memberService.isExistsEmail(snsReqDto.getEmail().trim())) {
-
-			MemberDto findedMember = memberService.findMemberByEmail(snsReqDto.getEmail().trim()); 
-			
-			//탈퇴(0)한 회원 또는 임시가입(9)된 회원인 경우
-			if(findedMember.getStatus() == 0 || findedMember.getStatus() == 9) {
-				findedMember.setStatus(1);
-				try {
-					findedMember.setPassword( EncryptAES256.encrypt(snsReqDto.getToken().trim()) );
-				} catch (Exception e) {
-					e.printStackTrace();
-					System.out.println("SNS 로그인 처리 중 비밀번호 암호화 실패1");
-				}
-			}
-			
-			//회원 sns로그인 테이블에서 조회
-			MemberSnsLoginsDto findedSnsMember = memberSnsLoginsService.getSnsMemberByEmail(snsReqDto.getEmail().trim());
-			
-			//sns회원 테이블에 존재시
-			if(findedSnsMember != null) {
-				findedSnsMember.setAccess_token(snsReqDto.getToken().trim());
-				findedSnsMember.setLogin_modified_at(LocalDateTime.now());
-			}
-			
-			//sns회원 테이블에 미존재시
-			else {
-				
-				findedSnsMember = MemberSnsLoginsDto.builder()
-											.memberDto(findedMember)
-											.login_id(snsReqDto.getEmail())
-											.provider(Commons.TEMPORARY)
-											//.provider_id(Commons.TEMPORARY) //유니크한 값
-											.access_token(snsReqDto.getToken())
-											.refresh_token(null)
-											.status(1)
-											.login_modified_at(LocalDateTime.now())
-											.login_created_at(LocalDateTime.now())
-											.build();
-				
-				//sns회원 테이블에 저장
-				findedSnsMember = memberSnsLoginsService.save(findedSnsMember);
-			}
-			
-			return findedSnsMember != null ? ResponseEntity.status(HttpStatus.CREATED).body(ResultUtil.success(findedSnsMember))
-					: ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultUtil.fail("sns회원 저장 실패"));
-			
-		}
-		
-		//회원 미 존재시
-		MemberDto memberDto = null;
-		try {
-			memberDto = MemberDto.builder()
-									.email(snsReqDto.getEmail().trim())
-									.password(EncryptAES256.encrypt(snsReqDto.getToken().trim()))
-									.nickname( snsReqDto.getEmail().trim().split("@")[0] )
-									.created_at(LocalDateTime.now())
-									.updated_at(LocalDateTime.now())
-									.status(1)
-									.build();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("SNS 로그인 처리 중 비밀번호 암호화 실패2");
-		}
-		//회원 테이블에 저장
-		MemberDto savedMember = memberService.join(memberDto);
-		//sns회원 테이블에 저장
-		MemberSnsLoginsDto snsDto = MemberSnsLoginsDto.builder()
-									.memberDto(savedMember)
-									.login_id(savedMember.getEmail())
-									.access_token(snsReqDto.getToken().trim())
-									.status(1)
-									.login_created_at(LocalDateTime.now())
-									.login_modified_at(LocalDateTime.now())
-									.provider(Commons.TEMPORARY)
-									//.provider_id(Commons.TEMPORARY) //유니크한 값
-									.build();
-		MemberSnsLoginsDto savedSnsMember = memberSnsLoginsService.save(snsDto);
-		
-		return savedSnsMember != null ? ResponseEntity.status(HttpStatus.CREATED).body(ResultUtil.success(savedSnsMember))
-				: ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResultUtil.fail("sns회원 저장 실패"));
-	} */
 	}
 	
 }
